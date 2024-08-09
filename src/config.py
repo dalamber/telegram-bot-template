60f32aa050703c9ec01e67bb8f246efb25aa08b2
Anton Shpakovsky <anton.shpakovsky@gmail.com>
2024-08-09 12:51:04 +0700

    Add dotenv, add ghcr publish

--- src/config.py
+++
@@ -1,6 +0,0 @@
-import os
-
-CFG_BOT_TOKEN = os.getenv("BOT_TOKEN")
-
-def print_current_config():
-    print(f"CFG_BOT_TOKEN: {CFG_BOT_TOKEN}")
--- src/main.py
+++ src/main.py
@@ -15,8 +15,9 @@ Press Ctrl-C on the command line or send a signal to the process to stop the
 bot.
 """
 
-import config
+from dotenv import load_dotenv
 import logging
+import os
 
 from telegram import ForceReply, Update
 from telegram.ext import Application, CommandHandler, ContextTypes, MessageHandler, filters
@@ -30,6 +31,9 @@ logging.getLogger("httpx").setLevel(logging.WARNING)
 
 logger = logging.getLogger(__name__)
 
+load_dotenv()
+
+CFG_BOT_TOKEN = os.getenv("BOT_TOKEN")
 
 # Define a few command handlers. These usually take the two arguments update and
 # context.
@@ -55,7 +59,7 @@ async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
 def main() -> None:
     """Start the bot."""
     # Create the Application and pass it your bot's token.
-    application = Application.builder().token(config.CFG_BOT_TOKEN).build()
+    application = Application.builder().token(CFG_BOT_TOKEN).build()
 
     # on different commands - answer in Telegram
     application.add_handler(CommandHandler("start", start))
